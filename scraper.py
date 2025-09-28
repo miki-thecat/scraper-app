6c802d90ee48dc02ba0afa4a55b1c705fc23c3fe
K.M <189582101+e1b23086@ users.noreply.github.com>
2025-09-28 06:17:04 +0000

    デブコンテナ上でのChrome / driver のパスの設定

--- scraper.py
+++ scraper.py
@@ -5,3 +5,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+
+# Dev Container でインストール済みの Chromium / chromedriver のパス
+CHROMIUM_BINARY = "/usr/bin/chromium"
+CHROMEDRIVER_BINARY = "/usr/bin/chromedriver"
+
+def 
+
