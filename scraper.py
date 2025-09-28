57f2a5476b8de32d1d716adce58a868e26abee5e
K.M <189582101+e1b23086@ users.noreply.github.com>
2025-09-28 05:57:38 +0000

    scrapeファイルを作成開始

---
+++ scraper.py
@@ -0,0 +1,7 @@
+import time
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
